5c13409503172969728378b76c9bdfa69669377c
aalexandr6 <aalexanderp6@gmail.com>
2023-05-01 01:39:26 -0500

    client JS and config

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,21 +1,26 @@
-import { openDB } from 'idb';
+//https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB
+//
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  //create the database
+  openDB("jate", 1, {
+    //upgrade the database if needed
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      //create a new object store
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => console.error("putDb not implemented");
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => console.error("getDb not implemented");
 
 initdb();
--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,11 +1,29 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
-
+window.addEventListener("beforeinstallprompt", (event) => {
+  //store the event for later use'
+  window.deferredPrompt = event;
+});
+// remove the hidden attribute from the install button
+butInstall.removeAttribute("hidden");
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener("click", async () => {
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    return;
+  }
+  // Show the install prompt
+  promptEvent.prompt();
+  // reset the deferred prompt variable, since
+  // prompt() can only be called once.
+  window.deferredPrompt = null;
+  // Hide the install button
+  butInstall.setAttribute("hidden", true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener("appinstalled", (event) => {});
+//clear the deferredPrompt so it can be garbage collected
+window.deferredPrompt = null;
